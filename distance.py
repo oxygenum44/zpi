837659041e4b0c3650d7324e04ef8c159d0f90ee
Adam Nowak <46918765+adnowak@users.noreply.github.com>
2020-03-16 13:30:28 +0100

    Fixed Jaccard

--- distance.py
+++ distance.py
@@ -25,7 +25,7 @@ def jaccard_distance(bag_of_words1, bag_of_words2):
             if word1 == word2:
                 sentences_common_set.add(word1)
 
-    return len(sentences_common_set) / len(sentences_sum_set)
+    return 1-(len(sentences_common_set) / len(sentences_sum_set))
 
 
 def euclidean_distance(bag_of_words1, bag_of_words2):
@@ -39,9 +39,8 @@ def euclidean_distance(bag_of_words1, bag_of_words2):
 
 
 def cosine_similarity(bag_of_words1, bag_of_words2):
-    denominator = (np.sqrt(np.dot(bag_of_words1, bag_of_words2)) * np.sqrt(np.dot(bag_of_words2, bag_of_words2)))
-    return np.dot(bag_of_words1, bag_of_words2) / denominator
-
+    denominator = np.dot(bag_of_words1, bag_of_words2)
+    return 1-(np.sqrt(np.dot(bag_of_words1, bag_of_words2)) * np.sqrt(np.dot(bag_of_words2, bag_of_words2))) / denominator
 
 def bag_of_words(tweets_list):
     all_words_set = set()
@@ -57,8 +56,7 @@ def bag_of_words(tweets_list):
     for tweet in tweets_list:
         bag_of_words = np.zeros(shape=len(all_words))
         for i in range(len(all_words)):
-            if all_words[i] in tweet:
-                bag_of_words[i] = 1
+            bag_of_words[i] += tweet.count(all_words[i])
         tweets_bags.append(bag_of_words.copy())
 
     return tweets_bags
