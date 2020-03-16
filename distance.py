3d1511bf8d72af992390f494db8cc484a8302832
Adam Nowak <46918765+adnowak@users.noreply.github.com>
2020-03-16 10:44:28 +0100

    Added files

---
+++ distance.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def jaccard_distance(sentence1, sentence2):
+    sentences_sum_set = set()
+    sentences_common_set = set()
+    for word in sentence1:
+        sentences_sum_set.add(word)
+    for word in sentence2:
+        sentences_sum_set.add(word)
+
+    for word1 in sentence1:
+        for word2 in sentence2:
+            if word1 == word2:
+                sentences_common_set.add(word1)
+
+    return len(sentences_common_set)/len(sentences_sum_set)
+
+def euclidean_distance(bag_of_words1, bag_of_words2):
+    root_range = len(bag_of_words1)
+
+    squared_subs_sum = 0
+    subtraction_list = bag_of_words1 - bag_of_words2
+    for i in range(0, root_range):
+        squared_subs_sum += subtraction_list[i]**2
+
+    return squared_subs_sum**(1.0/float(root_range))
+
+def cosine_similarity(sentence1, sentence2):
+    return np.dot(sentence1, sentence2) / (np.sqrt(np.dot(sentence1, sentence1)) * np.sqrt(np.dot(sentence2, sentence2)))
+
+def bag_of_words(tweets_list):
+    all_words_set = set()
+    all_words = list()
+    for tweet in tweets_list:
+        for word in tweet:
+            all_words_set.add(word)
+
+    for word in all_words_set:
+        all_words.append(word)
+
+    tweets_bags = list()
+    for tweet in tweets_list:
+        bag_of_words = np.zeros(shape=len(all_words))
+        for i in range(len(all_words)):
+            if all_words[i] in tweet:
+                bag_of_words[i] = 1
+        tweets_bags.append(bag_of_words.copy())
+
+    return tweets_bags
---
+++ zpi.py
@@ -0,0 +1,56 @@
+import distance
+import tweet_cleaner
+
+tweet_tokenizer = tweet_cleaner.TweetTokenizer()
+tweet_tokens = []
+
+print("Różnica miedzy uwzględnianiem hashtagow a nie, oba ze stemmerem")
+aa = tweet_cleaner.tweet_obrabiarka('I’m a Simple Simple Mans boycotts Man. I see things from China, I boycott. #boycottmulan I don’t need #WuhanCoronavirus #ChineseCoronavirus wtever you call it You created the virus and now people are suffering becoz of u', hashowac=0, stemmer=1)
+bb = tweet_cleaner.tweet_obrabiarka('I’m a HArd Time time time Mans boycotts Man. I see thing from Poland, I boycott. #boycottmulan I don’t need #WuhanCoronavirus #ChineseCoronavirus wtever you call it You created the virus and now people are suffering becoz of u', hashowac=1, stemmer=1)
+print(aa)
+print(bb)
+
+text1 = 'Poland :) goes little small good time times'
+text2 = 'Poland go battle small good pretime times'
+text3 = 'Poland go battle small good pretime times !!!!'
+text4 = 'Sweden was good small crab two times/22 :P'
+
+texts = [text1, text2, text3, text4]
+print()
+print("ZRODLOWE")
+print()
+print(texts)
+print()
+print("BEZ STEMMERA")
+print()
+z_stemmerem = []
+for sent in texts:
+    z_stemmerem.append(tweet_cleaner.tweet_obrabiarka(sent, hashowac=0, stemmer=0))
+
+print(z_stemmerem)
+
+print()
+print("Z STEMMEREM")
+print()
+bezstemmera = []
+for sent in texts:
+    bezstemmera.append(tweet_cleaner.tweet_obrabiarka(sent, hashowac=0, stemmer=1))
+print(bezstemmera)
+
+
+print()
+print("DYSTANS MIĘDZY TWEETAMI 1 I 2")
+print()
+bag_of_words = distance.bag_of_words(z_stemmerem)
+print("Tweet 1: "+str(z_stemmerem[0])+", BoW: "+str(bag_of_words[0]))
+print()
+print("Tweet 2: "+str(z_stemmerem[1])+", BoW: "+str(bag_of_words[1]))
+print()
+print("DYSTANS JACCARDA")
+print(distance.jaccard_distance(z_stemmerem[0], z_stemmerem[1]))
+print()
+print("DYSTANS EUKLIDESA")
+print(distance.euclidean_distance(bag_of_words[0], bag_of_words[1]))
+print()
+print("DYSTANS COSINE")
+print(distance.cosine_similarity(bag_of_words[0], bag_of_words[1]))
