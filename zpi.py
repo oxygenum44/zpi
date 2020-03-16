8502c5d9f3505385395f847e6788855625899b89
Michal Smolnicki <michal.jan.smolnicki@gmai.com>
2020-03-16 11:46:54 +0100

    Added tf-idf calculations

--- zpi.py
+++ zpi.py
@@ -1,21 +1,25 @@
 import distance
-import tweet_cleaner
+from tweet_cleaner import *
+from features import calculate_tf_idf
 
-tweet_tokenizer = tweet_cleaner.TweetTokenizer()
+tweet_tokenizer = TweetTokenizer()
 tweet_tokens = []
 
 print("Różnica miedzy uwzględnianiem hashtagow a nie, oba ze stemmerem")
-aa = tweet_cleaner.tweet_obrabiarka('I’m a Simple Simple Mans boycotts Man. I see things from China, I boycott. #boycottmulan I don’t need #WuhanCoronavirus #ChineseCoronavirus wtever you call it You created the virus and now people are suffering becoz of u', hashowac=0, stemmer=1)
-bb = tweet_cleaner.tweet_obrabiarka('I’m a HArd Time time time Mans boycotts Man. I see thing from Poland, I boycott. #boycottmulan I don’t need #WuhanCoronavirus #ChineseCoronavirus wtever you call it You created the virus and now people are suffering becoz of u', hashowac=1, stemmer=1)
+aa = tweet_obrabiarka('I’m a Simple Simple Mans boycotts Man. I see things from China, I boycott. #boycottmulan I don’t need #WuhanCoronavirus #ChineseCoronavirus wtever you call it You created the virus and now people are suffering becoz of u', hashowac=0, stemmer=1)
+bb = tweet_obrabiarka('I’m a HArd Time time time Mans boycotts Man. I see thing from Poland, I boycott. #boycottmulan I don’t need #WuhanCoronavirus #ChineseCoronavirus wtever you call it You created the virus and now people are suffering becoz of u', hashowac=1, stemmer=1)
 print(aa)
 print(bb)
 
-text1 = 'Poland :) goes little small good time times'
-text2 = 'Poland go battle small good pretime times'
+text1 = 'Poland :) goes little small small small small good time times ground'
+text2 = 'Poland  Poland Poland go battle battle small good pretime times'
 text3 = 'Poland go battle small good pretime times !!!!'
 text4 = 'Sweden was good small crab two times/22 :P'
 
 texts = [text1, text2, text3, text4]
+nowynowy = []
+for itr in texts:
+    nowynowy.append(tweet_obrabiarka(itr, hashowac=0, stemmer=1))
 print()
 print("ZRODLOWE")
 print()
@@ -25,7 +29,7 @@ print("BEZ STEMMERA")
 print()
 z_stemmerem = []
 for sent in texts:
-    z_stemmerem.append(tweet_cleaner.tweet_obrabiarka(sent, hashowac=0, stemmer=0))
+    z_stemmerem.append(tweet_obrabiarka(sent, hashowac=0, stemmer=0))
 
 print(z_stemmerem)
 
@@ -34,7 +38,7 @@ print("Z STEMMEREM")
 print()
 bezstemmera = []
 for sent in texts:
-    bezstemmera.append(tweet_cleaner.tweet_obrabiarka(sent, hashowac=0, stemmer=1))
+    bezstemmera.append(tweet_obrabiarka(sent, hashowac=0, stemmer=1))
 print(bezstemmera)
 
 
@@ -54,3 +58,8 @@ print(distance.euclidean_distance(bag_of_words[0], bag_of_words[1]))
 print()
 print("DYSTANS COSINE")
 print(distance.cosine_similarity(bag_of_words[0], bag_of_words[1]))
+
+
+print()
+print("Demnostracja tf_idf")
+print(calculate_tf_idf(sent=nowynowy[0], corpus=nowynowy))
